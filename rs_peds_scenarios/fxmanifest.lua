9cd06fe1f3dedcfaf3a17783a46dc25c434f44e9
riversafe33 <riversafe33@outlook.com>
2025-10-19 22:35:42 +0200

    Change author name in fxmanifest.lua

--- rs_peds_scenarios/fxmanifest.lua
+++ rs_peds_scenarios/fxmanifest.lua
@@ -3,7 +3,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 game 'rdr3'
 lua54 'yes'
 
-author 'alicain'
+author 'riversafe'
 description 'Comida para players con peds'
 version '1.0.0'
 
